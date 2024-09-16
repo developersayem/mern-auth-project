25d9a1e8f0bc5278c490b8a136b1b70fffaa3098
Molla Sayem <developersayem012@gmail.com>
2024-09-16 14:11:15 +0600

    [Done]Database-setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import dontenv from "dotenv"
+import { connectDB } from "./db/connectDB.js";
 
 
 
-
+dontenv.config()
 const app = express();
 
 
@@ -12,5 +14,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
+    connectDB()
     console.log('Server listening on port 3000!');
 });
