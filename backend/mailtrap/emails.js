a5fcd56e0943686712610c1a8e0a5a6b0a8b223f
Molla Sayem <developersayem012@gmail.com>
2024-09-17 00:39:06 +0600

    [Done]verify account & send welcome email

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -17,3 +17,22 @@ try {
     throw new Error (`Error sending verification email: ${error}`)
 }
 }
+
+export const sendWelcomeEmail=async(email,name)=>{
+    const recipient = [{email}] 
+    try {
+      const response=  await mailtrapClient.send({
+            from: sender,
+            to:recipient,
+            template_uuid: "6dffa186-7eca-48d3-a74a-8bfa1d9e098a",
+            template_variables: {
+                "name": name,
+                "company_info_name": "MERN Auth Company"
+              }
+        })
+        console.log("Email send successfully",response)
+    } catch (error) {
+        console.log("`Error sending verification email:",error)
+        throw new Error (`Error sending verification email: ${error}`)
+    }
+}
